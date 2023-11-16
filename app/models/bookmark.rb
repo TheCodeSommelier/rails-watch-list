9c5f2a6aeab50522d213fba0d08af4c28b4efa5c
Tony Masek <tm@tony-masek.com>
2023-11-16 11:12:30 +0000

    Created the validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,8 @@
 class Bookmark < ApplicationRecord
-  belongs_to :movies
-  belongs_to :lists
+  belongs_to :movie
+  belongs_to :list
 
-  validates :comment, length: { minimum: 6}
-  validates :movies, :lists, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :movie, :list, presence: true
+  validates_uniqueness_of :list_id, :scope => [:movie_id]
 end
